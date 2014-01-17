8dc5e965e07c4c525b88149d56beac7a54b08db2
Dragon Dave <dragon@scraperwiki.com>
2014-01-17 18:12:19 +0000

    better tests

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import regression
+import example_tests
 from nose.tools import assert_equal
 
 
@@ -14,3 +15,21 @@ class MyTestClass(object):
 def test_get_functions():
     functions = regression.functions_of_class(MyTestClass)
     assert_equal(set(functions), set(['test_function', 'setUpTest']))
+
+def test_get_functions_from_example():
+    functions = regression.functions_of_class(example_tests.ConvertTest)
+    assert_equal(set(functions), set([
+        'not_a_you_know_what',
+        'test_sector',
+        'test_caption_detail_always_total',
+        'test_first_row',
+        'test_commodities',
+        'test_last_row',
+        'test_measure',
+        'test_indicator',
+        'test_frequency_always_annual',
+        'test_all_figures_are_floats',
+        'test_dates',
+        'test_correct_number_of_rows',
+        'test_origin',
+        'test_source']))
